4eef13af74a7b9650dd4a49e9513b95994b9bc48
Vlad Shchigolev <vladshch.40@gmail.com>
2021-09-05 00:21:29 +0300

    some tasks added

--- task8.go
+++ task8.go
@@ -6,33 +6,31 @@ import (
 )
 
 func main() {
-	var x int64
+	var x int64 // переменная, значение которой будем менять
 
 	x = 255
-	fmt.Println("x =", x)
-	writeToBit(&x, 1, 0)
-	fmt.Println("Записали в 0-ой бит 1, x =", x) // 10 -> 11
+	fmt.Println("x value before modification (decimal):", x)
+	bitOverwrite(&x, 15, 1)
+	fmt.Printf("x value after modification (binary): %b\nx value after modification (decimal): %d\n", x, x)
 
-	//x = 3
-	//fmt.Println("x =", x)
-	//writeToBit(&x, 2, 1)
-	//fmt.Println("Записали в 2-ой бит 1, x =", x) // 011 -> 111
-	//
-	//x = 6
-	//fmt.Println("x =", x)
-	//writeToBit(&x, 1, 0)
-	//fmt.Println("Записали в 1-ой бит 0, x =", x) // 110 -> 100
+	fmt.Println("--------------------------")
+	x = 255
+	fmt.Println("x value before modification (decimal):", x)
+	bitOverwrite(&x, 4, 0)
+	fmt.Printf("x value after modification (decimal): %d", x)
 
 }
 
-// число, в какой бит записываем значение, значение
-func writeToBit(x *int64, i, v int) { // Функция имеет параметры: 1:указатель на исходное число (возвращать ничего не будем, модифицируем исходное значение), 2:индекс изменяемого бита (индексация справа налево), 3:на какое значение меняем
+func bitOverwrite(x *int64, i, v int) { // Функция имеет параметры: 1:указатель на исходное число (возвращать ничего не будем, модифицируем исходное значение), 2:индекс изменяемого бита (индексация справа налево), 3:на какое значение меняем
 	if v == 0 { // рассматривается 3 случая: если i-тый бит надо поменять на 0
-		temp := int64(math.MaxInt64 - 1<<i)
-		fmt.Printf("temp value: %b\nx value: %b\n", temp, *x)
-		*x = *x & temp
+		mask := int64(math.MaxInt64 - 1<<i) // смещаем 1-чку на i-ю позицию, используем "-" c операндом math.MaxInt64 слева, чтобы получить инверсию результата выражения 1<<i
+		//fmt.Println(1 << i)
+		fmt.Printf("mask value: %b\nx value before modification: %b\n", mask, *x)
+		*x = *x & mask // выполняем операцию побитового И
+		fmt.Printf("x value after modification: %b\n", *x)
 
 	} else if v == 1 { // если i-тый бит надо поменять на 1
+		fmt.Printf("x value before modificaton %b\n", *x)
 		*x = *x | 1<<i // Справа от | мы берём единицу и с помощью сдвига вправо (умножение на 2^i) переставляем её на нужную позицию.
 		// Далее выполняем операцию побитового ИЛИ над нашим значением и результатом сдвига
 	} else { // если в аргументе передано что-то отличное от 0 и 1.
